8c92e8f090b2892edd5743475feda722319ac347
5eqn <491100866@qq.com>
2024-01-13 19:16:21 +0800

    feat: func render

--- src/component/Header.tsx
+++ src/component/Header.tsx
@@ -7,16 +7,40 @@ import InputButton from "./InputButton"
 export default function Header(props: {
   depth: number,
   label: string,
+  validate: (name: string) => string | null
   onDelete: () => void
-  onAdd: (name: string) => string | null
+  onAdd: (name: string) => void
+  onWrapLet: (name: string) => void
+  onWrapFunc: () => void
+  onWrapPi: () => void
+  onWrapApp: () => void
 }) {
 
   return <div>
     <Slot depth={props.depth}>
       <Text text={props.label} />
       <Spacer />
-      <InputButton onConfirm={props.onAdd} />
-      <Button value="x" onClick={props.onDelete} />
+      <InputButton
+        onConfirm={props.onAdd}
+        validate={props.validate}
+      > + </InputButton>
+      <Button
+        danger
+        onClick={props.onDelete}
+      > x </Button>
+      <InputButton
+        onConfirm={props.onWrapLet}
+        validate={props.validate}
+      > L </InputButton>
+      <Button
+        onClick={props.onWrapFunc}
+      > F </Button>
+      <Button
+        onClick={props.onWrapPi}
+      > P </Button>
+      <Button
+        onClick={props.onWrapApp}
+      > A </Button>
     </Slot>
   </div>
 }
