29385c824833181dbaf19c44fe4132c27461e233
5eqn <491100866@qq.com>
2024-01-13 09:49:46 +0800

    del: views

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,9 @@ import { useImmer } from "use-immer";
 import Center from "./component/Center";
 import Text from "./component/Text";
 import { evaluate, pretty, quote, sample, Term, wrap } from "./model";
-import TermView from "./view/TermView";
 
 function App() {
-  const [state, setState] = useImmer<Term>(sample)
+  const [state, _] = useImmer<Term>(sample)
   const value = wrap(() => pretty(quote(0, evaluate([], state))))
   return <div style={{
     display: "flex",
@@ -22,13 +21,6 @@ function App() {
     <div style={{
       flexGrow: 1,
     }}>
-      <TermView
-        level={0}
-        value={state}
-        onChange={(updater) => setState(draft =>
-          updater(draft)
-        )}
-      />
     </div>
   </div>
 }
--- src/view/AnyView.tsx
+++
@@ -1,9 +0,0 @@
-import Slot from "../component/Slot"
-import Text from "../component/Text"
-import { CommonProps, Term } from "../model"
-
-export default function AnyView(props: CommonProps<Term>) {
-  return <Slot level={props.level}>
-    <Text text="Selector" />
-  </Slot>
-}
