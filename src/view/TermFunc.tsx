ea3fc001dacce4ffdd662870023d3c930045d223
5eqn <491100866@qq.com>
2024-01-14 18:10:51 +0800

    refact: separate func and pi infer

---
+++ src/view/TermFunc.tsx
@@ -0,0 +1,28 @@
+import { TermPropsBase } from ".";
+import Labeled from "../component/Labeled";
+import { i18n } from "../i18n";
+import { TFunc } from "../model/term";
+import { TermHeader } from "./TermHeader";
+
+export interface TermFuncProps extends TermPropsBase<TFunc> {
+  params: JSX.Element[]
+  body: JSX.Element
+}
+
+export function TermFunc(props: TermFuncProps) {
+  const { depth } = props.req
+  return <div>
+    <TermHeader
+      req={props.req}
+      type={props.type}
+      label={i18n.term.var}
+    />
+    {props.params}
+    <Labeled
+      depth={depth}
+      label={i18n.term.body}
+      children={props.body}
+    />
+  </div>
+}
+
