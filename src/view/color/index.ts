8c372ca6d7113ce6af015cc27f0ffead6d45c338
5eqn <subcat2077@gmail.com>
2024-01-25 14:03:42 +0800

    feat: update color

--- src/view/color/index.ts
+++ src/view/color/index.ts
@@ -1,10 +1,9 @@
 export const palette = {
   background: '#f3f3f3',
   neutral: '#f7f7f7',
-  light: '#fdfff7',
+  num: '#fdfff7',
   app: '#f3c6cb',
   pi: '#e7e4f8',
-  num: '#f9e6c8',
-  type: '#c7ffe0',
+  type: '#f9e6c8',
   func: '#fff9cd',
 }
